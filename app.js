051326d823e97734134f51bde9fc2310837c89da
Blackie24x <kacper.kraczkowski24@gmail.com>
2022-07-15 10:54:26 +0200

    deploy-rest-api-update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use("/api/notes", notesRoutes);
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3euv3wf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3euv3wf.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("work");
